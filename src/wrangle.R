fb4581a7fb1cb66e94b05b390e74a85102e7b467
Brianna Elliott <bwe2@duke.edu>
2020-02-20 15:30:50 -0500

    ...

--- src/wrangle.R
+++ src/wrangle.R
@@ -1,3 +1,3 @@
 # Minimal Wrangling of the data
 gmpts <- bind_rows(gm182, gm182UP)
-colnames(gmpts) <- c('trackNum', 'time', 'longitude', 'latitude', 'status')
+colnames(gmpts) <- c('trackNum', 'time', 'longitude', 'latitude', 'green')
